52e8f0da6c2d5f7122ab2fe5cdaa6f2612889a49
KhovetS2 <isaque.elis.escola@gmail.com>
2024-11-10 21:13:15 -0300

    feat: adiciona rota de pegar local by id

--- app/routers/location.py
+++ app/routers/location.py
@@ -1,28 +1,36 @@
 from fastapi import APIRouter, HTTPException
-from app.models.location_model import Location
+
 from app.database.firebase import get_db
-from app.services.location_services import exist_location_by_lat_long, exist_location_by_id, validate_location
+from app.models.location_model import Location
+from app.services.location_services import (
+    exist_location_by_id,
+    validate_location,
+)
 
 router = APIRouter()
 db = get_db()
 collection_ref = db.collection("localizacoes")
+
 
 @router.post("/add_location/")
 async def add_location(location: Location):
     try:
         # Verifica se as coordenadas são válidas
         if not validate_location(location.latitude, location.longitude):
-            raise HTTPException(status_code=400, detail= "Coordenadas inválidas. A latitude deve estar entre -90 e 90, e a longitude entre -180 e 180.")
-        
+            raise HTTPException(
+                status_code=400,
+                detail="Coordenadas inválidas. A latitude deve estar entre -90 e 90, e a longitude entre -180 e 180.",
+            )
+
         # Verifica se a localização com a latitude e longitude fornecidas já existe
-        if exist_location_by_lat_long(location.latitude, location.longitude):
-            raise HTTPException(status_code=409, detail="A localização já existe.")
+        # if exist_location_by_lat_long(location.latitude, location.longitude):
+        #    raise HTTPException(status_code=409, detail="A localização já existe.")
 
         data = {
             "nome": location.nome,
             "latitude": location.latitude,
             "longitude": location.longitude,
-            "isOffline": False
+            "isOffline": False,
         }
 
         # Adiciona os dados ao Firestore
@@ -30,18 +38,23 @@ async def add_location(location: Location):
 
         return {
             "id": doc_ref[1].id,  # Retorna o ID do documento recém-criado
-            "message": "Localização adicionada com sucesso!"
+            "message": "Localização adicionada com sucesso!",
         }
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erro ao adicionar localização: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao adicionar localização: {e}"
+        )
+
 
 @router.delete("/del_location/")
 def delete_location(id_location: str):
     try:
         # Verifica se a localização com o ID fornecido existe
         if not exist_location_by_id(id_location):
-            raise HTTPException(status_code=404, detail="Localização não encontrada no banco de dados.")
+            raise HTTPException(
+                status_code=404, detail="Localização não encontrada no banco de dados."
+            )
 
         collection_ref.document(id_location).delete()
 
@@ -49,8 +62,28 @@ def delete_location(id_location: str):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao deletar localização: {e}")
-    
-# Ivan Germano: Rota para buscar todas as localizações utilizando stream() que é uma função fornecida pela 
+
+
+# Ivan Germano: Rota para buscar todas as localizações utilizando stream() que é uma função fornecida pela
+# biblioteca do Firebase para o Firestore. Ele é utilizado para obter todos os documentos dentro de uma coleção.
+@router.get("/find_locations_by_id/{id}")
+async def find_locations_by_id(id: str):
+    try:
+        doc_ref = collection_ref.document(id)
+        doc = doc_ref.get()
+        location_data = doc.to_dict()
+        # Ivan Germano: Adiciona o ID do documento para permitir a manipulação dele no frontend
+        location_data["id"] = doc.id
+
+        # Ivan Germano: O print abaixo é para debug caso queira ver como os dados vem do firestore.
+        # print(locations)
+        return {"location": location_data}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar localização: {e}")
+
+
+# Ivan Germano: Rota para buscar todas as localizações utilizando stream() que é uma função fornecida pela
 # biblioteca do Firebase para o Firestore. Ele é utilizado para obter todos os documentos dentro de uma coleção.
 @router.get("/find_all_locations/")
 async def find_all_locations():
@@ -60,29 +93,34 @@ async def find_all_locations():
         for doc in docs:
             location_data = doc.to_dict()
             # Ivan Germano: Adiciona o ID do documento para permitir a manipulação dele no frontend
-            location_data["id"] = doc.id 
+            location_data["id"] = doc.id
             locations.append(location_data)
 
         # Ivan Germano: O print abaixo é para debug caso queira ver como os dados vem do firestore.
-        # print(locations) 
+        # print(locations)
         return {"locations": locations}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao buscar localizações: {e}")
 
-# Ivan Germano: Está é a rota que permite o usuário alterar o nome do local salvo   
+
+# Ivan Germano: Está é a rota que permite o usuário alterar o nome do local salvo
 @router.put("/edit_location_name")
 async def edit_location_name(id_location: str, new_name: str):
-
     try:
         # Ivan Germano: Verifica se a localização com o ID fornecido existe
         if not exist_location_by_id(id_location):
-            raise HTTPException(status_code=404, detail="Localização não encontrada no banco de dados.")
-        
+            raise HTTPException(
+                status_code=404, detail="Localização não encontrada no banco de dados."
+            )
+
         # Ivan Germano: Atualiza o nome do local no Firestore
         collection_ref.document(id_location).update({"nome": new_name})
-        
+
         return {"message": "Nome do local atualizado com sucesso!"}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Erro ao atualizar o nome do local: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Erro ao atualizar o nome do local: {e}"
+        )
+
